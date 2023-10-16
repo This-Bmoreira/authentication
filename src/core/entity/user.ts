459f04639b80d936d534553b1b0aa8a69cfd229a
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-10-16 10:15:52 -0300

    refactor: move the validation methods to the User entity

--- src/core/entity/user.ts
+++ src/core/entity/user.ts
@@ -1,4 +1,5 @@
-import { validateEmail, validateName, validatePassword } from '../../adapter/gateways/validator/validator-manager'
+// import { validateName, validatePassword } from '../../adapter/gateways/validator/validator-manager'
+import { ValidatorAdapter } from '../../external/lib/validator/validator-adapter'
 import { left, right, type Either } from '../../shared/either'
 import { Entity } from '../../shared/entity'
 import { InvalidEmailError } from '../error/invalid-email'
@@ -14,9 +15,9 @@ export class UserEntity extends Entity<UserData> {
   }
 
   static create (userData: UserData): Either<InvalidNameError | InvalidEmailError | InvalidPasswordError, UserEntity> {
-    const nameIsValid = validateName(userData.name)
-    const emailIsValid = validateEmail(userData.email)
-    const passwordIsValid = validatePassword(userData.password)
+    const nameIsValid = this.validateName(userData.name)
+    const emailIsValid = this.validateEmail(userData.email)
+    const passwordIsValid = this.validatePassword(userData.password)
 
     if (!nameIsValid) {
       return left(new InvalidNameError(userData.name))
@@ -34,7 +35,7 @@ export class UserEntity extends Entity<UserData> {
   }
 
   static updateUserWithNewName (data: UpdateNameData): Either<InvalidNameError, string> {
-    const isNameValid = validateName(data.newName)
+    const isNameValid = this.validateName(data.newName)
 
     if (!isNameValid) {
       return left(new InvalidNameError(data.newName))
@@ -51,7 +52,7 @@ export class UserEntity extends Entity<UserData> {
   }
 
   static updateUserWithNewPassword (data: UpdatePasswordData): Either<InvalidPasswordError, string> {
-    const isPasswordValid = validatePassword(data.newPassword)
+    const isPasswordValid = this.validatePassword(data.newPassword)
 
     if (!isPasswordValid) {
       return left(new InvalidPasswordError(data.newPassword))
@@ -65,5 +66,20 @@ export class UserEntity extends Entity<UserData> {
     const newPassword = updatedUserData.password
 
     return right(newPassword)
+  }
+
+  static validateName (name: string): boolean {
+    const nameValidator = new ValidatorAdapter()
+    return nameValidator.isName(name).isRight()
+  }
+
+  static validateEmail (email: string): boolean {
+    const emailValidator = new ValidatorAdapter()
+    return emailValidator.isEmail(email).isRight()
+  }
+
+  static validatePassword (password: string): boolean {
+    const passwordValidator = new ValidatorAdapter()
+    return passwordValidator.isPassword(password).isRight()
   }
 }
